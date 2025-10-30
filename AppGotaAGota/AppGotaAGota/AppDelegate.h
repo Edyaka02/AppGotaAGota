35a6222d44cd2cb55c0e908efbe48015819775aa
Edgar Manuel Carrillo Muruato <132968940+Edyaka02@users.noreply.github.com>
2025-10-29 18:06:18 -0700

    Add files via upload

---
+++ AppGotaAGota/AppGotaAGota/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppGotaAGota
+//
+//  Created by Guest User on 29/10/25.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
---
+++ AppGotaAGota/AppGotaAGota/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AppGotaAGota
+//
+//  Created by Guest User on 29/10/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
